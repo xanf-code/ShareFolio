67e84716cea208a56469734327ecee374d3f19a7
xanf-code <darshanaswath@gmail.com>
2021-03-20 23:22:55 +0530

    Profile

---
+++ lib/Pages/Wrapper/wrapper.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:my_app/Models/userDB.dart';
+import 'package:my_app/Pages/AuthPage/Login/login.dart';
+import 'package:my_app/Pages/HomePage/home.dart';
+import 'package:provider/provider.dart';
+
+class Wrapper extends StatelessWidget {
+  const Wrapper({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final user = Provider.of<UserModel>(context);
+
+    if (user == null) {
+      return LoginForm();
+    }
+    return MyHomePage();
+  }
+}
