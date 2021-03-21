17c182a396fb070aac9a53687f8721978525439b
xanf-code <darshanaswath@gmail.com>
2021-03-22 00:34:32 +0530

    Added context key

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:my_app/Models/userDB.dart';
 import 'package:my_app/Pages/HomePage/Settings/settings.dart';
@@ -44,6 +45,7 @@ class MyApp extends StatelessWidget {
       child: StreamProvider<UserModel>.value(
         value: AuthService().user,
         child: GetMaterialApp(
+          navigatorKey: Get.key,
           debugShowCheckedModeBanner: false,
           home: initScreen == 0 || initScreen == null
               ? OnBoarding()
