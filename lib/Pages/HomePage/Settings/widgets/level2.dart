41b2ee161c7e78fbd2fd3562309cdee750d7b58b
xanf-code <darshanaswath@gmail.com>
2021-03-26 15:31:41 +0530

    changed Imapct

--- lib/Pages/HomePage/Settings/widgets/level2.dart
+++ lib/Pages/HomePage/Settings/widgets/level2.dart
@@ -95,7 +95,7 @@ class _Level2State extends State<Level2> {
                     ),
                     GestureDetector(
                       onTap: () {
-                        HapticFeedback.mediumImpact();
+                        HapticFeedback.lightImpact();
                         isRecording == false ? recordAudio() : stopRecording();
                       },
                       child: Row(
